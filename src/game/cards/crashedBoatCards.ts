5bd68a0e12d88b015fa71036655f7418b31811d8
ZoxDev <mathias.bar1809@gmail.com>
2024-08-19 22:57:20 +0200

    feat: wip crashed card boat

--- src/game/cards/crashedBoatCards.ts
+++ src/game/cards/crashedBoatCards.ts
@@ -1,6 +1,152 @@
 import type { CardType, Rarity } from "../types";
 
+type EffectParams = {
+	playerCount: number;
+	food: number;
+	water: number;
+	wood: number;
+	bulletAmout: number;
+	hasRevolver: boolean;
+	playerOrder: any[];
+	voteCount: number;
+	hisPoisoned: boolean;
+};
+
 export type CrashedBoatCard = {
+	id: number;
 	rarity: Rarity;
 	type: CardType;
+	title: string;
+	description: string;
+	isPermanent?: boolean;
+	waterAmount?: number;
+	foodAmount?: number;
+	effect?: (params: EffectParams) => void;
 };
+
+export const crashedBoatCards: CrashedBoatCard[] = [
+	{
+		id: 1,
+		rarity: "rare",
+		type: "crashedBoat",
+		title: "Coconut",
+		description: "This coconut will give you 3 food",
+		foodAmount: 3,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 2,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Water bottle",
+		description: "A little potion of water that will give you 1 water",
+		waterAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+	{
+		id: 3,
+		rarity: "common",
+		type: "crashedBoat",
+		title: "Sandwich",
+		description: "A opened sandwich that will give you 1 food",
+		foodAmount: 1,
+	},
+];
+
+// 7 water bottle and 7 sandwich
+// 6 bullet 3 revolver
+// 5 useless card
